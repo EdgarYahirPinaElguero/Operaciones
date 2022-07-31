7f6d64c824a45a430010f31ae889fcc365efe142
EdgarYahirPinaElguero <89890681+EdgarYahirPinaElguero@users.noreply.github.com>
2022-07-30 19:54:05 -0500

    Create Operaciones.cpp

---
+++ Operaciones.cpp
@@ -0,0 +1,28 @@
+public class Operacion1 {
+	// Variables Globales
+	int suma;
+	int resta;
+	int multiplicacion;
+	int division;
+	
+	//Metodos
+	public int suma(int num1, int num2) {
+	suma = num1+num2;
+	return suma;
+	}
+	
+	public int resta(int num1, int num2) {
+	resta = num1-num2;
+	return resta;
+	}
+	
+	public int multiplicacion(int num1, int num2) {
+	multiplicacion = num1*num2;
+	return multiplicacion;
+	}
+	
+	public int division(int num1, int num2) {
+	division= num1/num2;
+	return division;
+	}
+}
